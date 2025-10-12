4aadb464216825bcbc1e63a1c55680862269f385
tushary03 <tushary2580@gmail.com>
2025-10-12 12:27:57 +0530

    second iteration of day 1 partice

--- Day-1 python-pratice.py
+++ Day-1 python-pratice.py
@@ -1,8 +1,31 @@
+import time
 
 # variables for input
 
-name = input("whats your name : ")
-age = input("How old are you : ")
-interest = input("what do you love doing the most : ")
+#name input and process dummy in between next age input
+name = input("Bot: whats your name \n You: ")
+time.sleep(0.5)
+print("Processing . . .")
+time.sleep(1)
 
-print(f"Awesome here's your short bio, your name is {name} and you age is {age}, your specially love {interest}")
+#age input and process dummy in between next interest input
+age = int(input("Bot: How old are you \n You: "))
+#conditional according to age of the user/guest
+if age <= 18:
+    print('Bot: Ah, a young explorer!')
+elif age <= 30:
+    print("Bot: Cool, right in your prime time!")
+else: print("Bot: Wow full of wisdom and experience")
+
+time.sleep(0.5)
+print("Processing . . .")
+time.sleep(1)
+
+#interest dummy and process dummy in between final print
+interest = input("Bot: what do you love doing the most \n You: ")
+time.sleep(0.5)
+print("Building your short bio . . .")
+time.sleep(1)
+
+#output for the small bio
+print(f"Awesome here's your short bio,\n 'your name is {name} and your age is {age},\n your specially love {interest}' ")
